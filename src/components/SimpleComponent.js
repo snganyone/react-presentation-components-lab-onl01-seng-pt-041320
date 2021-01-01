2d0b752956519321890214eabb533a057aae703a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-01-01 12:42:12 -0800

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,26 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            mood: "happy"
+        };
+    }
+
+    handleChange = () => {
+        this.setState({
+            mood: "sad"
+        });
+    }
+
+
+    render(){
+        return(
+            <div onClick={this.handleChange}>{this.state.mood}</div>
+        )
+    }
+}
+
+export default SimpleComponent;
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,12 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+const SimplerCompent = props => {
+    //this.props.handleClick = {};
+    return(
+    <div>I am just happy</div>
+    )
+}
+
+
+export default SimplerCompent;
